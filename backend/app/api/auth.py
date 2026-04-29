bfe284f7ad842cb0dfa8e6f2755f790d8daf8479
antimasingh070 <santima2290@gmail.com>
2026-04-29 16:33:46 +0530

    Implemented signup login JWT authentication

---
+++ backend/app/api/auth.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from backend.app.db.session import SessionLocal
+from backend.app.models.user import User
+from backend.app.schemas.user import UserCreate, UserLogin
+from backend.app.core.security import (
+    hash_password,
+    verify_password,
+    create_access_token
+)
+
+router = APIRouter(prefix="/auth", tags=["Auth"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/signup")
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    existing = db.query(User).filter(User.email == user.email).first()
+
+    if existing:
+        raise HTTPException(status_code=400, detail="Email already exists")
+
+    new_user = User(
+        full_name=user.full_name,
+        email=user.email,
+        hashed_password=hash_password(user.password),
+        role=user.role
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message": "User created successfully"}
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = db.query(User).filter(User.email == user.email).first()
+
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    if not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token(
+        {"sub": db_user.email, "role": db_user.role}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
 from backend.app.db.session import Base, engine
 from backend.app.models.user import User
+from backend.app.api.auth import router as auth_router
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="HireGenie AI")
 
+app.include_router(auth_router)
+
 @app.get("/")
 def home():
-    return {"message": "HireGenie AI Backend Running"}
+    return {"message": "HireGenie AI Running"}
